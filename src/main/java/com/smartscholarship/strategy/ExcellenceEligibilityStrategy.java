7164b9e6936b0c743763db56149d9e3c190a329c
Owen Choong <owenchoong1111@gmail.com>
2026-07-09 23:01:01 +0800

    Implement ExcellenceEligibilityStrategy

--- src/main/java/com/smartscholarship/strategy/ExcellenceEligibilityStrategy.java
+++ src/main/java/com/smartscholarship/strategy/ExcellenceEligibilityStrategy.java
@@ -1,4 +1,13 @@
 package com.smartscholarship.strategy;
 
+import com.smartscholarship.model.Student;
+
 public class ExcellenceEligibilityStrategy {
+    @Override
+    public boolean checkEligibility(Student student) {
+
+        return student.getGPA() >= 3.80 &&
+                student.getHouseholdIncome() <= 8000;
+
+    }
 }
